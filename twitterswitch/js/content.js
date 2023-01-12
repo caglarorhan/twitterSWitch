9b99e5b3cc323ad31b6b6ff834a7fbba9caadf81
caglarorhan <caglaror@gmail.com>
2023-01-12 12:24:44 -0600

    CTRL+click event listening feature

--- twitterswitch/js/content.js
+++ twitterswitch/js/content.js
@@ -4,6 +4,7 @@ window.addEventListener('load', ()=>{
 });
 const twitterSWitch ={
     init(){
+        this.catchTwits();
     },
     ioTypes:{
         hex:"hex",
@@ -14,13 +15,22 @@ const twitterSWitch ={
         let result='';
         switch(data.from) {
             case "hex":
+                if(!this.isThisHex(data.ingredient.substring(0,2))){
+                    result = data.ingredient;
+                    console.log('it is not a hex string');
+                    break;
+                }
                 switch(data.to) {
                     case "text":
                         let textED ='';
-                        data.incoming.forEach(h=>{
-                            textED+=String.fromCharCode(parseInt(h, 16));
-                        })
-                        result = textED;
+                            let hexED = data.ingredient.split(' ');
+                            //console.log(hexED);
+                            hexED.forEach(h=>{
+                                    textED+=String.fromCharCode(parseInt(h, 16));
+                            })
+                            result = textED;
+
+
                         break;
                 }
                 break;
@@ -28,7 +38,7 @@ const twitterSWitch ={
                 switch(data.to) {
                     case "hex":
                         let txtHex =[];
-                        data.incoming.split('').forEach(t=>{
+                        data.ingredient.split('').forEach(t=>{
                             txtHex.push(t.charCodeAt(0).toString(16));
                         })
 
@@ -37,16 +47,27 @@ const twitterSWitch ={
                         break;
                 }
                 break;
-
-
-
         }
         return result;
+    },
+    isThisHex(testThis){
+        let A = parseInt(testThis,16).toString(16);
+        let B = testThis.toLowerCase();
+        console.log(A,B);
+        return A===B;
     },
     catchTwits(){
         document.querySelectorAll('[data-testid="tweetText"] :not([data-visited="true"])').forEach(twt=>{
             twt.dataset.visited="true";
-            twt.innerHTML=this.convert({ingredient: twt.innerHTML,from: this.ioTypes.text,to: this.ioTypes.hex})
+            //twt.innerHTML=this.convert({ingredient: twt.innerHTML,from: this.ioTypes.text,to: this.ioTypes.hex})
+            twt.style.cursor="pointer";
+            twt.title = "Use CTRL+click to convert hex to text on this tweet.";
+            twt.addEventListener("click",(e)=>{
+                if(e.ctrlKey && twt.dataset.hexed!=="true"){
+                    twt.innerHTML=this.convert({ingredient: twt.textContent,from: this.ioTypes.hex,to: this.ioTypes.text})
+                    twt.dataset.hexed="true";
+                }
+            })
         })
     }
 
